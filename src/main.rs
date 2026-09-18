507becd1620d6cb3c50a441ceea79180d0a19a65
Gaizka <soy@gaizka.blog>
2026-09-18 19:54:03 +0200

    ✨ Resolver parte 2 del día 2

--- src/main.rs
+++ src/main.rs
@@ -89,6 +89,56 @@ fn find_invalid_numbers_part_1_optimized((start, end): (usize, usize)) -> Vec<us
     numbers
 }
 
+/// Siguiendo un poco la idea de la parte 1 optimizada
+fn find_invalid_numbers_part_2(start: usize, end: usize, part_size: u32) -> Vec<usize> {
+    if start.digits() % part_size != 0 {
+        let start = 10_usize.pow(start.digits());
+        return find_invalid_numbers_part_2(start, end, part_size);
+    }
+
+    // Al menos se tiene que repetir dos veces el número, así que con
+    // números cuyo dígito es el mismo que el part_size, falla
+    if start.digits() == part_size {
+        let start = 10_usize.pow(part_size);
+        return find_invalid_numbers_part_2(start, end, part_size);
+    }
+
+    // Antes de quitar un dígito a la cota final, mejor comprobar
+    // que la cota sea correcta.
+    if start > end {
+        return vec![];
+    }
+
+    if end.digits() % part_size != 0 {
+        let end = 10_usize.pow(end.digits() - 2);
+        return find_invalid_numbers_part_2(start, end, part_size);
+    }
+
+    let mut num = start;
+    let mut candidates = vec![];
+
+    while num <= end {
+        let digit_count = num.digits();
+        let step = 10_usize.pow(digit_count - part_size);
+
+        let first_part = num / step;
+
+        let candidate =
+            first_part * step
+            + (0..(digit_count/part_size - 1))
+                .map(|p| first_part * 10_usize.pow(part_size * p))
+                .sum::<usize>();
+
+        if candidate >= start && candidate <= end {
+            candidates.push(candidate);
+        }
+
+        num = (first_part + 1) * step;
+    }
+
+    candidates
+}
+
 fn parse_range(range: &str) -> (usize, usize) {
     let parsed: Vec<&str> = range.split('-').collect();
 
@@ -120,6 +170,30 @@ fn run_day2_part_1_optimized(line: &str) -> usize {
         .sum()
 }
 
+fn run_day2_part_2(line: &str) -> usize
+{
+     line.split(',')
+        .map(|r| parse_range(r))
+        .map(|(start, end)| {
+            let digits = end.digits();
+
+            let mut result = (1..digits)
+                .filter(|s| digits % s == 0)
+                .map(|s| find_invalid_numbers_part_2(start, end, s))
+                .flatten()
+                .collect::<Vec<usize>>();
+
+            // Un número cuyos dígitos sean todos iguales, será válido
+            // para todos los divisores de la cantidad de dígitos
+            result.sort();
+            result.dedup();
+
+            result
+        })
+        .flatten()
+        .sum()
+}
+
 fn main() {
     let my_exercise_input = "197-407,262128-339499,557930-573266,25-57,92856246-93001520,2-12,1919108745-1919268183,48414903-48538379,38342224-38444598,483824-534754,1056-1771,4603696-4688732,75712519-75792205,20124-44038,714164-782292,4429019-4570680,9648251-9913729,6812551522-6812585188,58-134,881574-897488,648613-673853,5261723647-5261785283,60035-128980,9944818-10047126,857821365-857927915,206885-246173,1922-9652,424942-446151,408-1000";
 
@@ -128,6 +202,9 @@ fn main() {
 
     let result_optimized = run_day2_part_1_optimized(my_exercise_input);
     println!("Parte 1, optimizado: {result_optimized}");
+
+    let result = run_day2_part_2(my_exercise_input);
+    println!("Parte 2, optimizado: {result}");
 }
 
 #[cfg(test)]
@@ -153,6 +230,7 @@ mod tests {
     #[test]
     fn exercise_example_test() {
         assert_eq!(run_day2_part_1(EXERCISE_EXAMPLE), 1227775554);
+        assert_eq!(run_day2_part_2(EXERCISE_EXAMPLE), 4174379265);
     }
 
     #[test]
@@ -162,5 +240,11 @@ mod tests {
         assert_eq!((3 as usize).ilog10() + 1, 1);
         assert_eq!((12 as usize).ilog10() + 1, 2);
         assert_eq!((131 as usize).ilog10() + 1, 3);
+    }
+
+    #[test]
+    fn find_invalid_numbers_part_2_test() {
+        assert_eq!(find_invalid_numbers_part_2(998, 1012, 1), [999]);
+        assert_eq!(find_invalid_numbers_part_2(998, 1012, 2), [1010]);
     }
 }
